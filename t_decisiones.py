ef9d7e9af5c9738a3f9566d609d4f9081a9f43c7
JesusGt12 <58190735+JesusGt12@users.noreply.github.com>
2021-04-05 16:53:38 -0500

    Update t_decisiones.py

--- t_decisiones.py
+++ t_decisiones.py
@@ -1,5 +1,5 @@
 print ("Toma de deciciones")
-print("Ingrese los datos de la primera alternativa\n")
+print("Ingrese los datos de la primera alternativa \n")
 x1 = float(input("Ingresa cantidad a ganar:\t"))
 p1 = float(input("Ingresa probabilidad de ganar:\t"))
 x2 = float(input("Ingresa cantidad a perder:\t"))
@@ -10,13 +10,13 @@ p11 = float(input("Ingresa probabilidad de ganar:\t"))
 x21 = float(input("Ingresa cantidad a perder:\t"))
 p21 = float(input("Ingresa probabilidad de perder:\t"))
 
-print("\nOpcion 1 :\nGanar ", x1," con ", p1, " de probabilidad o perder ", x2, " con ", p2, " de probabilidad ")
-print("\nOpcion 2 :\nGanar ", x11," con ", p11, " de probabilidad o perder ", x21, " con ", p21, " de probabilidad ")
+print("\nOpcion 1 : \nGanar ", x1," con ", p1, " de probabilidad o perder ", x2, " con ", p2, " de probabilidad ")
+print("\nOpcion 2 : \nGanar ", x11," con ", p11, " de probabilidad o perder ", x21, " con ", p21, " de probabilidad ")
 
 s1 = ((x1*(p1/100)) - (x2*(p2/100)))
 s2 = ((x11*(p11/100)) - (x21*(p21/100))) 
 
 if(s1>s2):
-    print("\nLa mejor Toma de Decision es la numero 1")
+    print("\n La mejor Toma de Decision es la numero 1")
 else:
-    print("\nLa mejor Toma de Decision es la numero 2")
+    print("\n La mejor Toma de Decision es la numero 2")
